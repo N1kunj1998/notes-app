3b63370d3dff7267260ec740a6ce6a239dacf7e3
N1kunj1998 <njkhakhkhar@gmail.com>
2025-05-07 10:52:02 +0530

    added basic error handling, crud operations, logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,26 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+
+import notesRoute from "./routes/notes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoute);
+
+app.use((req, res, next) => {
+    next(Error("Endpoint not found"));
 });
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred";
+    if(error instanceof Error) errorMessage = error.message;
+    res.status(500).json({ error: errorMessage });
+})
 
 export default app;
