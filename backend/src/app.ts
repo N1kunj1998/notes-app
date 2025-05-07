ed808b36ded9e453b6e266a4f62417c6f7624023
N1kunj1998 <njkhakhkhar@gmail.com>
2025-05-07 13:01:53 +0530

    added proper error handline using http-error, improved code and handled mongoose error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 
 import notesRoute from "./routes/notes";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
@@ -13,14 +14,18 @@ app.use(express.json());
 app.use("/api/notes", notesRoute);
 
 app.use((req, res, next) => {
-    next(Error("Endpoint not found"));
+    next(createHttpError(404, "Endpoint not found"));
 });
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
     let errorMessage = "An unknown error occurred";
-    if(error instanceof Error) errorMessage = error.message;
-    res.status(500).json({ error: errorMessage });
+    let statusCode = 500;
+    if(isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
 })
 
 export default app;
